f18a6ea2779ebd3e707ede1f80bab7921a9a1968
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2025-11-14 10:03:21 -0300

    Realizando alteraçoes na navBar

--- projetoDw/src/homePaste/navbarPaste/Navbar.jsx
+++ projetoDw/src/homePaste/navbarPaste/Navbar.jsx
@@ -1,11 +1,22 @@
-import './navbar.css'
+import "./navbar.css";
 
-function Navbar(){
-    return(
-        <div className='navbarContainer'>
-            
-        </div>
-    )
+function Navbar() {
+  return <div className="navbarContainer"></div>;
+
+  const menuIcon = document.querySelector("#menuIcon");
+  const closeIcon = document.querySelector("#closeIcon");
+  const sidebar = document.querySelector("#sidebar");
+
+  menuIcon.addEventListener("click", showSidebar);
+  closeIcon.addEventListener("click", hideSidebar);
+
+  function showSidebar() {
+    sidebar.style.right = "0";
+  }
+
+  function hideSidebar() {
+    sidebar.style.right = "-100%";
+  }
 }
 
-export default Navbar
+export default Navbar;
