a8918e9e87b4d529abeafee17858b65ebe6bcb0f
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2025-12-12 09:45:55 -0300

    supabase login

--- projetoDw/src/supabase.js
+++ projetoDw/src/supabase.js
@@ -6,17 +6,27 @@ const KEY = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(URL, KEY);
 
 
+
 export async function registerUser(email, senha, nome) {
-  const { data, error } = await supabase.auth.signUp(
-    { email, password: senha },
-    { data: { nome } }
-  );
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password: senha,
+    options: {
+      data: {
+        nome: nome
+      }
+    }
+  });
+
   return { data, error };
 }
+
+
 export async function loginUser(email, senha) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password: senha,
   });
+
   return { data, error };
 }
